e0355e7154321e3527c34db397b14e3270ebf492
tiagowelsing <tiagowelsing@gmail.com>
2025-05-12 08:41:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def analisar_lista(lista):
     if not lista:  # Verifica se a lista está vazia
         return "A lista está vazia"
     
-    soma = sum(lista)
+    quantidade_pares = len([num for num in lista if num % 2 == 0])
+    
     max_num = max(lista)
     min_num = min(lista)
-    media = soma / len(lista)
+    media = sum(lista) / len(lista)
     
     return {
-        "soma": soma,
+        "quantidade_pares": quantidade_pares,
         "max": max_num,
         "min": min_num,
         "media": media
