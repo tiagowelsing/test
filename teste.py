c93215e1ddd7c6cf4313d2c616d0d72d974b485b
tiagowelsing <tiagowelsing@gmail.com>
2025-05-12 08:42:56 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,11 @@
-from main import analisar_lista
+rom funcao import analisar_lista
 
-numeros = [10, 23, 5, 8, 12, 33, 42, 7, 19, 28, 3, 16, 9, 50, 21]
+numeros = [10, 23, 5, 8, 12, 33, 42, 7, 19, 28, 3, 16, 9, 50, 21
 
 resultado = analisar_lista(numeros)
 
 print("Resultados da análise:")
-print(f"Soma: {resultado['soma']}")
+print(f"Média: {resultado['media']}")
 print(f"Máximo: {resultado['max']}")
 print(f"Mínimo: {resultado['min']}")
-print(f"Média: {resultado['media']}")
+print(f"Quantidade de números pares: {resultado['quantidade_pares']}")
